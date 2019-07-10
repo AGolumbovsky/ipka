eebedc66796c4bfe40bcb9640aed672f61761b3e
Andriy Golumbovsky <agolumbovsky@gmail.com>
2019-07-10 09:15:27 -0400

    clean up

--- ipka/Program.cs
+++ ipka/Program.cs
@@ -17,21 +17,6 @@ namespace ConsoleTest
                 Console.WriteLine("Some kind of na error or some shit: " + ex.Message);
             }
 
-           /* String strHostName = string.Empty;
-            // Getting Ip address of local machine...
-            // First get the host name of local machine.
-            strHostName = Dns.GetHostName();
-            Console.WriteLine("Local Machine's Host Name: " + strHostName);
-            // Then using host name, get the IP address list..
-            IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
-            IPAddress[] addr = ipEntry.AddressList;
-
-            Console.WriteLine("This is only a test, no need to panic! But if you must panic, please do, not like we all don't have a good reason");
-
-            for (int i = 0; i < addr.Length; i++)
-            {
-                Console.WriteLine("IP Address {0}: {1} ", i, addr[i].ToString());
-            }*/
             Console.ReadLine(); 
         }
     }
