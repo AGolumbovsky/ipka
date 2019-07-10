580fdab0d4ee3617afde30bb7f9fcbc187c77738
Andriy Golumbovsky <agolumbovsky@gmail.com>
2019-07-10 15:12:41 -0400

    started w/ cmd args

--- ipka/Program.cs
+++ ipka/Program.cs
@@ -6,18 +6,24 @@ namespace ConsoleTest
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
             try
             {
-                Process.Start("netsh interface ip set address 'Ethernet 6' 10.10.10.55 255.255.255.0 10.10.10.1");
+                string addr = "10.10.10.56";
+
+                Console.WriteLine("the first argument is: " + args[0]);
+
+                Console.WriteLine("netsh interface ip set address 'Ethernet 6'  {0} 255.255.255.0 10.10.10.1", addr);
+
+                Process.Start("netsh interface ip set address 'Ethernet 6' {addr} 255.255.255.0 10.10.10.1");
             }
             catch(Exception ex)
             {
                 Console.WriteLine("Some kind of na error or some shit: " + ex.Message);
             }
 
-            Console.ReadLine(); 
+            Console.ReadLine();
         }
     }
 }
